6327e6023c4400bdd533c9c4f7bafc8122748c63
DmitrySlv <dscreateapp@gmail.com>
2023-05-24 19:08:23 +0400

    Добавление Extensions.kt функции на проверку разрешения получения сведений о местоположений из фрагментов. Добавление логики в MainFragment.kt на проверку получения сведений о местоположении в приложении.

--- app/src/main/java/com/dscreate_app/weatherdata/fragments/MainFragment.kt
+++ app/src/main/java/com/dscreate_app/weatherdata/fragments/MainFragment.kt
@@ -1,12 +1,17 @@
 package com.dscreate_app.weatherdata.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.dscreate_app.weatherdata.R
 import com.dscreate_app.weatherdata.databinding.FragmentMainBinding
+import com.dscreate_app.weatherdata.utils.isPermissionGranted
 
 class MainFragment : Fragment() {
 
@@ -14,12 +19,32 @@ class MainFragment : Fragment() {
     private val binding: FragmentMainBinding
         get() = _binding ?: throw RuntimeException("FragmentMainBinding is null")
 
+    private lateinit var permLauncher: ActivityResultLauncher<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        permLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(requireContext(), "Permission is: $it", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            permLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/dscreate_app/weatherdata/utils/Extensions.kt
@@ -0,0 +1,10 @@
+package com.dscreate_app.weatherdata.utils
+
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.isPermissionGranted(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+}
