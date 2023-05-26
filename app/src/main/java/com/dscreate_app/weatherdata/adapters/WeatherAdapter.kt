57fe2dc77cac74d2a967386e2557d278541d7d5a
DmitrySlv <dscreateapp@gmail.com>
2023-05-26 19:30:49 +0400

    Создание WeatherAdapter ч2

--- app/src/main/java/com/dscreate_app/weatherdata/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/dscreate_app/weatherdata/adapters/WeatherAdapter.kt
@@ -1,23 +1,29 @@
 package com.dscreate_app.weatherdata.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.dscreate_app.weatherdata.R
 import com.dscreate_app.weatherdata.databinding.ListItemBinding
 import com.dscreate_app.weatherdata.models.WeatherModel
 
-class WeatherAdapter: ListAdapter<WeatherModel, ViewHolder>(DiffWeatherAdapter) {
+class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>(DiffWeatherAdapter) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return Holder(view)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.setData(getItem(position))
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class Holder(view: View): ViewHolder(view) {
        private val binding = ListItemBinding.bind(view)
+
         fun setData(weatherModel: WeatherModel) = with(binding) {
             tvDate.text = weatherModel.time
             tvCondition.text = weatherModel.condition
--- app/src/main/java/com/dscreate_app/weatherdata/fragments/HoursFragment.kt
+++ app/src/main/java/com/dscreate_app/weatherdata/fragments/HoursFragment.kt
@@ -5,15 +5,73 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dscreate_app.weatherdata.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dscreate_app.weatherdata.adapters.WeatherAdapter
+import com.dscreate_app.weatherdata.databinding.FragmentHoursBinding
+import com.dscreate_app.weatherdata.models.WeatherModel
 
 class HoursFragment : Fragment() {
+
+    private var _binding: FragmentHoursBinding? = null
+    private val binding: FragmentHoursBinding
+        get() = _binding ?: throw RuntimeException("FragmentHoursBinding is null")
+    private lateinit var weatherAdapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        _binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initRcView() = with(binding) {
+        rcViewHours.layoutManager = LinearLayoutManager(requireContext())
+        weatherAdapter = WeatherAdapter()
+        rcViewHours.adapter = weatherAdapter
+        val tempList = listOf(
+            WeatherModel(
+                "",
+                "12:00",
+                "Солнечно",
+                "+25°С",
+                "",
+                "",
+                "",
+                ""
+            ),
+            WeatherModel(
+                "",
+                "13:00",
+                "Солнечно",
+                "+30°С",
+                "",
+                "",
+                "",
+                ""
+            ),
+            WeatherModel(
+                "",
+                "14:00",
+                "Солнечно",
+                "+35°С",
+                "",
+                "",
+                "",
+                ""
+            )
+        )
+        weatherAdapter.submitList(tempList)
     }
 
     companion object {
